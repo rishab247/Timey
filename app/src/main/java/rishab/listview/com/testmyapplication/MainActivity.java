6b1d4151e6fb2c5fe2d646c50c95e43c85617448
Rishab aggarwal <Rishabaggarwal247@gmail.com>
2019-03-07 15:08:50 +0530

    rename

--- app/src/main/java/rishab/listview/com/testmyapplication/MainActivity.java
+++ app/src/main/java/rishab/listview/com/testmyapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package rishab.listview.com.testmyapplication;
 
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.sdsmdg.harjot.crollerTest.Croller;
@@ -10,14 +13,41 @@ import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener;
 
 public class MainActivity extends AppCompatActivity {
         private TextView txt;
+        private Croller croller;
+    private Button but;
+
+    final float startSize = 10; // Size in pixels
+    final float endSize = 40;
+    final int animationDuration = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+            setcroller();
+            txt=findViewById(R.id.textView) ;
+            but = findViewById(R.id.button2);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt=findViewById(R.id.textView2);
+                txt.setTextSize(40);
+                croller.setMax(59);
+            }
+        });
 
 
-        Croller croller = (Croller) findViewById(R.id.croller);
+
+    }
+
+
+
+
+
+
+
+    void setcroller(){
+         croller = (Croller) findViewById(R.id.croller);
         croller.setIndicatorWidth(10);
         croller.setMin(0);
         croller.setLabel("");
@@ -31,9 +61,8 @@ public class MainActivity extends AppCompatActivity {
         croller.setIndicatorColor(Color.parseColor("#0B3C49"));
         croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
 
-         txt=findViewById(R.id.textView) ;
-         croller = (Croller) findViewById(R.id.croller);
-         croller.setOnProgressChangedListener(new Croller.onProgressChangedListener() {
+        croller = (Croller) findViewById(R.id.croller);
+        croller.setOnProgressChangedListener(new Croller.onProgressChangedListener() {
             @Override
             public void onProgressChanged(int progress) {
                 // use the progress
@@ -43,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-            croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
+        croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
             @Override
             public void onProgressChanged(Croller croller, int progress) {
                 // use the progress
