f6469f85ef84365f0585738634482e69a73be3a8
SAURAV SUMAN <appmaker121@gmail.com>
2019-03-21 01:47:21 +0530

    math activity completed

--- app/src/main/java/rishab/listview/com/testmyapplication/math_activity.java
+++ app/src/main/java/rishab/listview/com/testmyapplication/math_activity.java
@@ -2,12 +2,80 @@ package rishab.listview.com.testmyapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class math_activity extends AppCompatActivity {
+    private TextView mathnotext,mathdifficulty;
+    private SeekBar seekBar1,seekBar2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_activity);
+        seekBar1=findViewById(R.id.seekbarno);
+        mathnotext=findViewById(R.id.mathnotext);
+        seekBar2=findViewById(R.id.seekBardifficulti);
+        mathdifficulty=findViewById(R.id.mathdifficulti);
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 1:
+                        mathnotext.setText("20+40");
+                        break;
+                    case 2:
+                        mathnotext.setText("20+309+40");
+                        break;
+                    case 3:
+                        mathnotext.setText("29*20+4");
+                        break;
+                    case 5:
+                        mathnotext.setText("45=900+.....");
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"hey you are startimg",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"hey you are stoped",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+      seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+          @Override
+          public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+              mathdifficulty.setText("difficulty level "+seekBar2.getProgress()+"/"+seekBar2.getMax());
+
+          }
+
+          @Override
+          public void onStartTrackingTouch(SeekBar seekBar) {
+              Toast.makeText(getApplicationContext(),"hey you are startimg",Toast.LENGTH_SHORT).show();
+
+          }
+
+          @Override
+          public void onStopTrackingTouch(SeekBar seekBar) {
+              Toast.makeText(getApplicationContext(),"hey you are stoped",Toast.LENGTH_SHORT).show();
+
+          }
+      });
+
+
+
+
+
+
+
     }
 }
